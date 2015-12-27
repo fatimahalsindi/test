d89fe67ea62ccfddbc435d4f02b8719e8f1c49d9
fatimahalsindi <fatimah_o@hotmail.com>
2015-12-27 11:01:00 +0000

    Update registerform.php

--- registerform.php
+++ registerform.php
@@ -17,58 +17,58 @@ include ("functions/function.php");
         <ul class="adventures"><?php get_adventure(); ?>
         </ul>
         <div id="register">
-        <form action=" " method="post" >
-        <table class="register_form">
-            <tr>
-                <td>
-                    <input type="text" name="fname" placeholder="First Name" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="text" name="lname" placeholder="Last Name" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="text" name="country" placeholder="Country" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="text" name="email" placeholder="Email Address" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="password" name="passw" placeholder="********************" />
-                </td>
-            </tr>
-
-            <tr>
-                <td>
-                    <input type="date" name="bday" placeholder="Date of Birth dd/mm/year"/>
-                </td>
-            </tr>
-            <tr>
-                <th>Choose your preferred role</th>
-            </tr>
-            <tr>
-                <td>
-                    <select name="role">
-                        <option>Reader</option>
-                        <option>Author</option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
+            <form action=" " method="post" >
+                        <table class="register_form">
+                                    <tr>
+                                                <td>
+                                                            <input type="text" name="fname" placeholder="First Name" />
+                                                </td>
+                                    </tr>
+                                    <tr>
+                                                 <td>
+                                                            <input type="text" name="lname" placeholder="Last Name" />
+                                                </td>
+                                    </tr>
+                                    <tr>
+                                                <td>
+                                                        <input type="text" name="country" placeholder="Country" />
+                                                    </td>
+                                    </tr>
+                                    <tr>
+                                                    <td>
+                                                        <input type="text" name="email" placeholder="Email Address" />
+                                                    </td>
+                                    </tr>
+                                    <tr>
+                                                    <td>
+                                                        <input type="password" name="passw" placeholder="********************" />
+                                                    </td>
+                                    </tr>
+                        
+                                    <tr>
+                                                    <td>
+                                                        <input type="date" name="bday" placeholder="Date of Birth dd/mm/year"/>
+                                                    </td>
+                                    </tr>
+                                    <tr>
+                                        <th>Choose your preferred role</th>
+                                    </tr>
+                                    <tr>
+                                                    <td>
+                                                        <select name="role">
+                                                            <option>Reader</option>
+                                                            <option>Author</option>
+                                                        </select>
+                                                    </td>
+                                    </tr>
+                                    <tr>
                 <td>
                     <button type="submit" name="register">Register</button>
                 </td>
     </tr>
         </table>
  </form>
-            <?php register(); ?>
+<?php register(); ?>
         </div>
     </div>
     </div>
