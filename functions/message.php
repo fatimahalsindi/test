843cc014c9dfc5b411ed4f09ed0c962854f77b4d
fatimahalsindi <fatimah_o@hotmail.com>
2015-12-27 10:25:30 +0000

    Update message.php

--- functions/message.php
+++ functions/message.php
@@ -10,16 +10,16 @@ if (isset($_POST['send'])) {
     if ($us_na == '') {
         echo 'Only logged in users can post comments.<a href="../index.php">log in here</a>';
     } else {
-        $sql_sender = "SELECT userID FROM USERS WHERE firstname='$us_na'";
+        $sql_sender = "SELECT userID FROM users WHERE firstname='$us_na'";
         $run_sql = mysqli_query($conn, $sql_sender);
        $data = mysqli_fetch_array($run_sql);
         $userid = $data['userID'];
 
-        $get_recipient = mysqli_query($conn, "SELECT userID FROM USERS WHERE userID= '$ad_id'");
+        $get_recipient = mysqli_query($conn, "SELECT userID FROM users WHERE userID= '$ad_id'");
         $rec = mysqli_fetch_array($get_recipient);
         $recipient = $rec['userID'];
         echo $recipient;
-            $i_message = "INSERT INTO Messages (message, userID, recipient, date) VALUES ('$message', '$userid', '$recipient', 'NOW')";
+            $i_message = "INSERT INTO messages (message, userID, recipient, date) VALUES ('$message', '$userid', '$recipient', 'NOW')";
             $run_message = mysqli_query($conn, $i_message);
 
         if ($run_message){
